60bf672aaf21ec61a040f765b03718ae744e0c77
Rachelle Williams <Rachellewms@gmail.com>
2018-01-19 06:07:55 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+ alert("Hello! Welcome to the Page!")
+
+ var visitorName = prompt("What is your name?");
+
+ var visitorFavcolor = prompt("What is your favorite color?");
+
+document.write("Hello " + visitorName + ", your favorite color is " + visitorFavcolor);
+
+console.log("Program Complete");
